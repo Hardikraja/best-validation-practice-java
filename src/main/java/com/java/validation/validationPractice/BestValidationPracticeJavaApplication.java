869c54d92a366d901478a1cb292740bc67913322
Hardikraja <hardikraja20@gmail.com>
2024-03-16 17:23:28 +0530

    init commit

---
+++ src/main/java/com/java/validation/validationPractice/BestValidationPracticeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.java.validation.validationPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestValidationPracticeJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestValidationPracticeJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/validation/validationPractice/BestValidationPracticeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.validation.validationPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestValidationPracticeJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
